3597fc86355c0f87012ec034c7792e80071eae8c
HxshPatil <156885581+hxsh01@users.noreply.github.com>
2024-02-02 13:18:26 +0530

    changes

--- script.js
+++ script.js
@@ -1,8 +1,66 @@
-function showConsole(event) {
-    event.preventDefault();
+const form =document.getElementById('form');
+const username = document.getElementById('name');
+const email = document.getElementById('mail');
+const number = document.getElementById('number');
+let nameFlag=0;
+let emailFlag=0;
+let numberflag=0;
 
-    function display(){
-        alert("data submitted");
-    }
-    document.getElementById("btn").addEventListener("click", display);
+function showConsole(){
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+    
+        validateInputs();
+    });
+    
+    const validateInputs = () => {
+        const isValidEmail = email => {
+            const check_e = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+            return check_e.test(String(email).toLowerCase());
+        }
+        
+        const isValidName = username => {
+            const check_n = /^([A-Za-z]{1,20})([ ]{0,1})([A-Za-z]{1,20})?([ ]{0,1})?([A-Za-z]{1,20})$/;
+            return check_n.test(String(username).toLowerCase());
+        }
+        
+        const isValidNum = number => {
+            const check_num = /^([6-9]{1}[0-9]{9})$/;
+            return check_num.test(String(number));
+        }
+            const nameValue = username.value.trim();
+            const emailValue = email.value.trim();
+            const numberValue = number.value.trim();
+    
+        if(nameValue === '') {
+            alert('Username is required');
+        }  else if (!isValidName(nameValue)) {
+            alert('Please check the name for errors');
+        } else{
+            nameflag=1;
+        }
+    
+        if(emailValue === '') {
+            alert('Username is required');
+        }  else if (!isValidEmail(emailValue)) {
+            alert('Please check the name for errors');
+        } else{
+            emailflag=1;
+        }
+    
+        if(numberValue === '') {
+            alert('Username is required');
+        }  else if (!isValidNumber(numberValue)) {
+            alert('Please check the name for errors');
+        } else{
+            numberflag=1;
+        }
+    
+        if(numberflag && emailFlag && nameFlag){
+            alert('form submitted')
+        }
+    };
+    
 }
+
+
